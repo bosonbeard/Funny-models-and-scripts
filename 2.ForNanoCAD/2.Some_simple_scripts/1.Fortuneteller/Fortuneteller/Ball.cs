30ee529a178269749ddf4d153c7d0e2fc5de6485
bosonbeard <bosonbeard@gmail.com>
2018-01-29 00:11:14 +0300

    add Fortuneteller
    it's a raw not release version (v. 0.5)

---
+++ 2.ForNanoCAD/2.Some_simple_scripts/1.Fortuneteller/Fortuneteller/Ball.cs
@@ -0,0 +1,172 @@
+﻿// Version 0.1
+//Use Microsoft .NET Framework 4 and MultiCad.NET API 7.0
+//Class for demonstrating the capabilities of MultiCad.NET
+//Assembly for the Nanocad 8.5 SDK is recommended (however, it is may be possible in the all 8.Х family)
+//Link imapimgd, mapimgd.dll and mapibasetypes.dll from SDK
+//Link System.Windows.Forms and System.Drawing
+//The commands: draws a fortune-teller ball 
+//This code in the part of non-infringing rights Nanosoft can be used and distributed in any accessible ways.
+//For the consequences of the code application, the developer is not responsible.
+
+//More detailed - https://habrahabr.ru/post/347720/
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing;
+using Multicad.Runtime;
+using Multicad.DatabaseServices;
+using Multicad.Geometry;
+using Multicad.CustomObjectBase;
+using Multicad;
+
+using Multicad.AplicationServices;
+using System.Threading;
+
+namespace Fortuneteller
+{
+    [CustomEntity(typeof(Ball), "2e814ea6-f1f0-469d-9767-269fedb32226", "Ball", "Fortuneteller Ball for NC85 Entity")]
+    [Serializable]
+    public class Ball : McCustomBase
+    {
+
+        private Point3d _basePnt = new Point3d(0, 0, 0);
+        double _radius=300;
+        int _shakes = 0;
+        string _text = "...";
+
+        public List<String> predictions =
+             new List<String>()
+             {"Act now!",
+              "Do not do this!",
+              "Maybe",
+              "I dont know",
+              "Everything is unclear",
+              "Yes!",
+              "No!",
+              "Take rest"
+             };
+
+
+        [CommandMethod("DFTBall", CommandFlags.NoCheck | CommandFlags.NoPrefix)]
+        public void DrawDoor()
+        {
+            Ball ball = new Ball();
+            ball.PlaceObject();
+        }
+
+        public override void OnDraw(GeometryBuilder dc)
+        {
+            dc.Clear();
+            dc.Color = McDbEntity.ByObject;
+            dc.DrawCircle(_basePnt, _radius);
+            dc.DrawCircle(_basePnt, _radius/2.0);
+            dc.TextHeight = 31;
+            dc.DrawMText(_basePnt, Vector3d.XAxis, _text, HorizTextAlign.Center, VertTextAlign.Center, _radius / 2.05);
+         
+        }
+
+
+
+        public override hresult PlaceObject(PlaceFlags lInsertType)
+        {
+            InputJig jig = new InputJig();
+
+            // Get the first box point from the jig
+            InputResult res = jig.GetPoint("Select center point:");
+            if (res.Result != InputResult.ResultCode.Normal)
+                return hresult.e_Fail;
+
+            _basePnt = res.Point;
+
+            // Add the object to the database
+            DbEntity.AddToCurrentDocument();
+
+            return hresult.s_Ok;
+        }
+
+
+        public override void OnTransform(Matrix3d tfm)
+        {
+            // To be able to cancel(Undo)
+            McUndoPoint undo = new McUndoPoint();
+            undo.Start();
+
+            // Get the coordinates of the base point and the rotation vector
+            this.TryModify();
+            this._basePnt = this._basePnt.TransformBy(tfm);
+            undo.Stop();
+        }
+
+
+        public override bool GetGripPoints(GripPointsInfo info)
+        {
+            info.AppendGrip(new McSmartGrip<Ball>(_basePnt+new Vector3d(0, _radius,0), (obj, g, offset) => {
+                obj.TryModify();
+                obj._basePnt += offset;
+                obj.TryModify();
+                obj.ShakePredict();
+            }));
+          
+            
+            var ctxGrip = new McSmartGrip<Ball>(McBaseGrip.GripType.PopupMenu, 2, _basePnt - 1.0 * new Vector3d(_radius, 0, 0),
+                                            McBaseGrip.GripAppearance.PopupMenu, 0, "Select menu", System.Drawing.Color.Lime);
+            ctxGrip.GetContextMenu = (obj, items) =>
+            {
+                items.Add(new ContextMenuItem("Shake Ball", "none", 1));
+                items.Add(new ContextMenuItem("Edit predictions", "none", 2));
+            };
+            ctxGrip.OnCommand = (obj, commandId, grip) =>
+            {
+                if (grip.Id == 2)
+                {
+                    switch (commandId)
+                    {
+
+                        case 1:
+                            {
+                                ShakePredict();
+                                break;
+                            }
+                        case 2:
+                            {
+
+                                ListEditorForm frm = new ListEditorForm(this);
+                                frm.Lpredictions.Items.AddRange(predictions.ToArray());
+                                frm.ShowDialog();
+                                break;
+
+                            }
+                    }
+                }
+            };
+            info.AppendGrip(ctxGrip);
+            return true;
+        }
+
+        private void ShakePredict()
+        {
+            Random rand = new Random();
+            int val = rand.Next(0, predictions.Count-1);
+            this.TryModify();
+            _text = predictions[val];
+
+        }
+
+
+   
+    }
+
+
+
+
+
+}
+
+
+
---
+++ 2.ForNanoCAD/2.Some_simple_scripts/1.Fortuneteller/Fortuneteller/ListEditorForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fortuneteller
+{
+    public partial class ListEditorForm : Form
+    {
+        private Ball ball;
+
+        public ListEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        public ListEditorForm(Ball ball)
+        {
+            this.ball = ball;
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DelBtn_Click(object sender, EventArgs e)
+        {
+            if (Lpredictions.SelectedItem !=null)
+            {
+                Lpredictions.Items.Remove(Lpredictions.SelectedItem);
+            }
+        }
+
+
+
+        private void AdBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text!="" | textBox.Text != " ")
+            {
+
+                Lpredictions.Items.Add(textBox.Text);
+            }
+
+        }
+
+        private void SaceBtn_Click(object sender, EventArgs e)
+        {
+            ball.predictions = Lpredictions.Items.OfType<String>().ToList();
+            this.Close();
+        }
+    }
+}
